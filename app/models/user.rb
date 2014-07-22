ef3a2420cd2c7b1da85ab7a335fa5f0a0e4d6e20
AnkurVyas-BTC <ankur.vyas@beaverlogic.com>
2014-07-22 16:52:33 +0530

    Modified User Model specs :  Added more specs for validating passwords.  Added specs for the blocked attribute.  Refactored error checks.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,19 @@
 class User < ActiveRecord::Base
-  #constants
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  VALID_PASSWORD_REGEX = /(?=.*\d.*)(?=.*[A-Z].*)(?=.*[!#@\$%&\?].*)(?=.*[a-z].*).{8,20}/
+  VALID_PASSWORD_REGEX = /((?=.*\d.*)(?=.*[A-Z].*)(?=.*[!#@\$\*%&\?].*)(?=.*[a-z].*)).{8,20}/
 
   validates_presence_of :first_name , message: "is required"
   validates_presence_of :last_name  , message: "is required"
   validates_presence_of :gender     , message: "must be selected"  
-  validates_inclusion_of :gender    , in: %w( M F ), message: "must be M or F"
+  validates_inclusion_of :gender, in: %w( M F ), message: "must be M or F"
 
   validates :email , presence: true,
                      uniqueness: true,
                      format: { with: VALID_EMAIL_REGEX }
 
-  VALID_PASSWORD_REGEX = /((?=.*\d.*)(?=.*[A-Z].*)(?=.*[!#\@\$%&\?].*)(?=.*[a-z].*)).{8,20}/
-  validates_presence_of :password   , message: "is required"
-  validates :password, length: { minimum:8,maximum:20 },
-                       format: { with: VALID_PASSWORD_REGEX }
+  validates_presence_of :password  , message: "is required"
+  validates_length_of   :password  , minimum: 8, message: "should be more than 8 characters" 
+  validates_length_of   :password  , maximum: 20, message: "should be less than 20 characters"
+  validates_format_of   :password  , with: VALID_PASSWORD_REGEX , message: "should have atleast 1 capital letter, 1 special symbol and 1 number"
   validates_presence_of :country_id , message: "must be selected"
 end 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,7 @@ describe User do
     let!(:errors) { user.save; user.errors }
 
     it "cannot be blank" do
-      expect(errors).to_not be_empty
+      expect(errors[:first_name]).to_not be_empty
     end  
 
     it "when blank shows user-friendly message" do
@@ -28,7 +28,7 @@ describe User do
     let!(:errors) { user.save; user.errors }
 
     it "cannot be blank" do 
-      expect(user.errors).to_not be_empty
+      expect(user.errors[:last_name]).to_not be_empty
     end
 
     it "when blank shows user-friendly message" do
@@ -40,7 +40,7 @@ describe User do
     let!(:errors) { user.save; user.errors }
 
     it "cannot be blank" do 
-      expect(errors).to_not be_empty
+      expect(errors[:gender]).to_not be_empty
     end
 
     it "when not selected shows user-friendly message" do
@@ -49,24 +49,23 @@ describe User do
 
 
     it "must have only one character" do      
-      user.gender= "M" * 1    
-      user.save    
+      user.gender= "M" * 1 
+      user.save       
       expect(errors[:gender]).to be_empty
     end
 
     it "must be either F or M" do
       user.gender = "A"
+      user.save
       expect(errors[:gender]).to include("must be M or F")
     end
-
   end
 
-  context "#email", check: true do
-    
+  context "#email" do  
 
     it "email cannot be blank " do
       user.save   
-      expect(user.errors).to_not be_empty
+      expect(user.errors[:email]).to_not be_empty
     end
 
     it "email already taken" do
@@ -74,26 +73,23 @@ describe User do
                       last_name: "Vyas", 
                       gender: "M",
                       email: "ankurvy1@gmail.com",
-                      password: "Ajay12@!",
+                      password: "Ankur12@",
                       blocked: "false",
                       country_id: "1"
                      )
       user.save
       expect(user.errors).to be_empty
-
-
       
       other_user = User.new(first_name:"Ambika",
                       last_name: "Bhatt", 
                       gender: "F",
                       email: "ankurvy1@gmail.com",
-                      password: "Ajay12@!",
+                      password: "Ankur12@",
                       blocked: "false",
                       country_id: "1"
                       )
       
-      other_user.save
-      
+      other_user.save      
       expect(other_user.errors).to_not be_empty
     end
 
@@ -105,45 +101,49 @@ describe User do
     end
   end
 
-  context "password" do
+  context "#password" do
     let!(:errors) { user.save; user.errors }
 
     it "cannot be blank" do      
-      expect(errors).to_not be_empty
+      expect(errors[:password]).to_not be_empty
     end
 
     it "when blank shows custom message " do      
       expect(errors[:password]).to include("is required")
     end
 
+    context "is invalid" do
+      it "when have less than 8 characters" do
+        expect(errors[:password]).to include("should be more than 8 characters")
+      end
 
-    it "must be of minimum 8 characters" do
-      expect(user.errors).to_not be_empty
+      it "when have more than 20 characters" do
+        user.password = "a"*21
+        user.save
+        expect(errors[:password]).to include("should be less than 20 characters")
+      end 
+
+      it "when does not contain 1 captial letter, special symbol or number" do
+        user.password = "abc"
+        user.save
+        expect(errors[:password]).to include("should have atleast 1 capital letter, 1 special symbol and 1 number")        
+      end
     end
 
-    it "must be of maximum 20 characters" do
-      user.password = "a" * 21
-      user.save
-      expect(user.errors).to_not be_empty 
+    context "is valid" do
+      it "when have exact 1 captial letter, 1 special symbol and 1 number " do
+        user.password = 'Ankurr1t@'
+        user.save      
+        expect(user.errors[:password]).to be_empty
+      end
+
+      it "when have more than 1 captial letter, special symbol or number" do
+        user.password = "AAnkur12!@"
+        user.save
+        expect(user.errors[:password]).to be_empty
+      end
     end
 
-    it "must have atleast 1 captial letter, 1 Special symbol and 1 number " do
-      user.password = "Ajay12@!"
-      user.save
-      expect(user.errors[:password]).to be_empty
-    end
-
-    it "when have more than 1 captial letter, special symbol or number" do
-      user.password = "Ajay12@!"
-      user.save
-      expect(user.errors[:password]).to be_empty
-    end
-
-    it "when dosen't contain 1 captial letter, special symbol or number" do
-      user.password = "abcdefg"
-      user.save
-      expect(user.errors).to_not be_empty
-    end
   end
 
 
@@ -151,13 +151,12 @@ describe User do
     let!(:errors) { user.save; user.errors}
 
     it "cannot be blank" do    
-      expect(errors).to_not be_empty
+      expect(errors[:country_id]).to_not be_empty
     end
 
     it "when not selected shows user-friendly message" do   
       expect(errors[:country_id]).to include("must be selected")
     end
-
   end
 
   context "#blocked" do
